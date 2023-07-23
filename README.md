dde0a4c4970fc1c02a8a1643b30aebde53fa3ea8
Vaishnavi Dungarwal <70143436+Vaishnavi-Dungarwal@users.noreply.github.com>
2023-07-23 15:52:33 +0400

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 "# Housing-Society" 
-"# Housing-Society" 
---
+++ index.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Housing Society</title>
+	<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css">
+
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+<!-- Popper JS -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.6/umd/popper.min.js"></script>
+
+<!-- Latest compiled JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js"></script>
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+	<meta name="keywords" content="Housing Society, WOW Slider, Easy Slider jQuery Plugin, Date Slider jQuery" />
+	<meta name="description" content="Housing Society created with WOW Slider, a free wizard program that helps you easily generate beautiful web slideshow" />
+	<!-- Start WOWSlider.com HEAD section -->
+	<link rel="stylesheet" type="text/css" href="engine1/style.css" media="screen" />
+	<style type="text/css">a#vlb{display:none}</style>
+	<script type="text/javascript" src="engine1/jquery.js"></script>
+</head>
+<body>
+<div class="container">
+	
+	<center><h2><span style="color: #FF9933">Housing</span>&nbsp;<span style="color: #000080">Society</span>&nbsp;<span style="color: green">Management</span></h2></center>
+
+
+
+	<div id="wowslider-container1">
+	<div class="ws_images">
+<a href="#"><img src="data1/images/vaasturamnagarhousingsocietyinbhosarielevationphoto1tdt.jpg" alt="Gokuldham" title="Gokuldham" id="wows0"/></a>
+<a href="#"><img src="data1/images/shosing_1.jpg" alt="Vaidehi" title="Vaidehi" id="wows1"/></a>
+<a href="#"><img src="data1/images/h.jpg" alt="Sahara" title="Sahara" id="wows2"/></a>
+<a href="#"><img src="data1/images/hhh.jpg" alt="Amrutdham" title="Amrutdham" id="wows3"/></a>
+</div>
+<div class="ws_bullets"><div>
+<a href="#wows0" title="Gokuldham"><img src="data1/tooltips/vaasturamnagarhousingsocietyinbhosarielevationphoto1tdt.jpg" alt="Gokuldham"/>1</a>
+<a href="#wows1" title="Vaidehi"><img src="data1/tooltips/shosing_1.jpg" alt="Vaidehi"/>2</a>
+<a href="#wows2" title="Sahara"><img src="data1/tooltips/h.jpg" alt="Sahara"/>3</a>
+<a href="#wows3" title="Amrutdham"><img src="data1/tooltips/hhh.jpg" alt="Amrutdham"/>4</a>
+</div></div>
+<a style="display:none" href="http://wowslider.com">Accordion Slider jQuery by WOWSlider.com v1.7</a>
+	</div>
+
+
+
+<div class="row">
+
+	<div class="col-md-4">
+	  
+		<a href="Owner/index.php"><center><img src="Images/admin.png"></center>
+		<h2 style="text-align: center;">Admin</h2></a>
+	</div>
+
+	<div class="col-md-4">
+	  
+		<a href="Society/index.php"><center><img src="Images/admin.png"></center>
+		<h2 style="text-align: center;">Society</h2></a>
+	</div>
+
+
+	<div class="col-md-4">
+	  
+		<a href="User/index.php"><center><img src="Images/admin.png"></center>
+		<h2 style="text-align: center;">User</h2></a>
+	</div>
+
+</div>
+</div>
+<script type="text/javascript" src="engine1/script.js"></script>
+</body>
+</html>
